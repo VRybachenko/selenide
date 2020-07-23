2713d00b37b2dd0d981e2f0f2098a757ac74217d
Vadym Rybachenko <vrybachenko@playtika.com>
2020-07-23 21:13:08 +0300

    first test with Selenide

--- src/main/java/com/mainacad/Lab.java
+++ src/main/java/com/mainacad/Lab.java
@@ -1,41 +1,79 @@
 package com.mainacad;
 
+import java.security.Key;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static com.codeborne.selenide.Selectors.byCssSelector;
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 /**
  * Hello world!
- *
  */
-public class Lab
-{
-    public static void main( String[] args )
-    {
+public class Lab {
+    public static void main(String[] args) throws InterruptedException {
 
-        //TODO Открыть тестовую страницу: https://www.toolsqa.com/automation-practice-form
-
-        //TODO: Вывести в консоль текст Инфо сообщения
-
-        //TODO: Вывести в консоль текст заголовка формы ("Practice Automation Form")
-
-        //TODO: Кликнуть линк Partial Link Test
-
-        //TODO: Заполнить поле First name:
-
-        //TODO: Заполнить поле Last name:
-
-        //TODO: Выбрать пол
-
-        //TODO: Выбрать количество лет опыта
-
-        //TODO: Заполнить поле дата
-
-        //TODO: Выбрать несколько Automation Tool
-
-        //TODO: Выбрать континент из выпадающего списка
-
-        //TODO: Выбрать несколько вариантов из списка Selenium Commands
-
-        //TODO: Кликнуть на кнопку Button
-
-        //TODO: Закрыть браузер
-
+        open("https://demoqa.com/automation-practice-form");
+        ///Заполняем поле firstNameField
+        $(byId("firstName")).setValue("TestTestovich");
+        Thread.sleep(1000);
+        //Заполняем поле lastName
+        $(byId("lastName")).setValue("TestTestovich");
+        Thread.sleep(1000);
+        //Заполняем поле Email
+        $(byId("userEmail")).setValue("Testtestovich@gmail.com");
+        Thread.sleep(1000);
+        //Выбираем Checkbox
+        $(byXpath("//*[@id='gender-radio-1']/..")).click();
+        Thread.sleep(1000);
+        //Заполняем поле Mobile
+        $(byId("userNumber")).setValue("1234567890");
+        Thread.sleep(1000);
+        //Открываем календарь
+        $(byId("dateOfBirthInput")).click();
+        Thread.sleep(1000);
+        //Выбираем месяц
+        $(byCssSelector(".react-datepicker__tab-loop"));
+        $(byCssSelector(".react-datepicker__month-select")).click();
+        $(byCssSelector(".react-datepicker__month-select"));
+        $(byCssSelector("[value='9']")).click();
+        Thread.sleep(1000);
+        //Выбираем год
+        $(byCssSelector(".react-datepicker__year-select")).click();
+        $(byCssSelector(".react-datepicker__year-select"));
+        $(byCssSelector("[value='1984']")).click();
+        Thread.sleep(1000);
+        //Выбираем день
+        $(byCssSelector(".react-datepicker__week:nth-child(3) .react-datepicker__day:nth-child(5)")).click();
+        Thread.sleep(1000);
+        //Выбираем Subjects
+        $(byXpath("/html//input[@id='subjectsInput']")).sendKeys("Eng");
+        $(byXpath("/html//input[@id='subjectsInput']")).sendKeys(Keys.ENTER);
+        Thread.sleep(1000);
+        //Выбираем хобби спорт
+        $(byXpath("//label[contains(.,'Sports')]")).click();
+        Thread.sleep(1000);
+        //Заполняем адресс
+        $(byId("currentAddress")).setValue("4 Privet Drive, Surrey");
+        Thread.sleep(1000);
+        //Выбираем значение для State
+        $(byXpath("/html//input[@id='react-select-3-input']")).sendKeys("Har");
+        $(byXpath("/html//input[@id='react-select-3-input']")).sendKeys(Keys.ENTER);
+        Thread.sleep(1000);
+        //Выбираем значение для CITY
+        $(byXpath("/html//input[@id='react-select-4-input']")).sendKeys("Kar");
+        $(byXpath("/html//input[@id='react-select-4-input']")).sendKeys(Keys.ENTER);
+        Thread.sleep(3000);
+        //Нажимаем на кнопку SUBMIT
+        $(byId("submit")).click();
+        Thread.sleep(1000);
+        //Нажимаем на кнопку Close
+        $(byId("closeLargeModal")).click();
     }
 }
